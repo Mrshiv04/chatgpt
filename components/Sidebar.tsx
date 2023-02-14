8785170cafa990241fec41fabc5a89c7e7587d4f
Shivam Shah <shiva.sean27@gmail.com>
2023-02-14 23:44:18 +0530

    implement logout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useSession, signOut } from 'next-auth/react';
 import NewChat from './NewChat';
 
 function Sidebar() {
+	const { data: session } = useSession();
 	return (
 		<div className='p-2 flex flex-col h-screen'>
 			<div className='flex-1'>
@@ -10,6 +14,14 @@ function Sidebar() {
 					{/* map through chat rows */}
 				</div>
 			</div>
+			{session && (
+				<img
+					onClick={() => signOut()}
+					src={session.user?.image!}
+					alt='userImage'
+					className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
+				/>
+			)}
 		</div>
 	);
 }
