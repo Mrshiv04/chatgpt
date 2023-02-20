99d30697abd969cc78e9d763d11d0ae6ba04d45f
Shivam Shah <shiva.sean27@gmail.com>
2023-02-20 21:46:26 +0530

    after adding models

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import { collection, orderBy, query } from 'firebase/firestore';
 import { db } from '../firebase';
 import ChatRow from './ChatRow';
+import ModelSelection from './ModelSelection';
 
 function Sidebar() {
 	const { data: session } = useSession();
@@ -18,18 +19,27 @@ function Sidebar() {
 			)
 	);
 
-	console.log(chats);
-
 	return (
 		<div className='p-2 flex flex-col h-screen'>
 			<div className='flex-1'>
 				<div>
 					<NewChat />
-					<div>{/* modelselection */}</div>
-					{/* map through chat rows */}
-					{chats?.docs.map((chat) => (
-						<ChatRow key={chat.id} id={chat.id} />
-					))}
+					<div className='hidden sm:inline'>
+						<ModelSelection />
+					</div>
+					<div className='flex flex-col space-y-2 my-2'>
+						{/* map through chat rows */}
+
+						{loading && (
+							<div className='animate-pulse text-center text-white'>
+								<p>Loading Chats...</p>
+							</div>
+						)}
+
+						{chats?.docs.map((chat) => (
+							<ChatRow key={chat.id} id={chat.id} />
+						))}
+					</div>
 				</div>
 			</div>
 			{session && (
